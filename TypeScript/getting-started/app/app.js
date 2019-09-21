46b798a6cc46bd3de85332761ec039256b20d168
robiharid <robiharid@gmail.com>
2019-09-21 23:29:33 +0100

    Load init TS script

---
+++ TypeScript/getting-started/app/app.js
@@ -0,0 +1,5 @@
+function startGame() {
+    var messageElement = document.getElementById("messages");
+    messageElement.innerText = "Welcome to MathScript! Starting new game...";
+}
+document.getElementById("startGame").addEventListener("click", startGame);
